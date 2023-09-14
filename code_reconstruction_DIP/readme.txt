d8f14d159ac646ebd7c850a7b2620a4ac6174227
wizard1203 <956234606@qq.com>
2023-09-15 00:06:23 +0800

    new

--- code_reconstruction_DIP/readme.txt
+++ code_reconstruction_DIP/readme.txt
@@ -1,3 +1,8 @@
 1. Download the dataset first, and put it under the "test_img" subdirectory.
 2. Download the pretrained models.
 3. Run "python reconstruction.py --data ./test_img --which_layer layer4 --lr 0.001 --max_iter 3000"
+
+
+python reconstruction.py --which_layer layer4 --lr 0.001 --max_iter 3000
+python reconstruction.py --which_layer layer4 --lr 0.001 --max_iter 1000
+
--- code_reconstruction_DIP/reconstruction.py
+++ code_reconstruction_DIP/reconstruction.py
@@ -19,6 +19,9 @@ import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
 from skip import skip
 
+from efficient_loader import efficient_load_partition_data_cifar10
+from resnet_v2 import ResNet18
+
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
@@ -156,126 +159,257 @@ def get_params(opt_over, net, net_input, downsampler=None):
 
     return params
 
+def load_my_state_dict(self, state_dict):
+    own_state = self.state_dict()
+    #print(state_dict)
+    for name, param in state_dict.items():
+        if name not in own_state:
+            print(name)
+            continue
+        #print(param.data.shape)
+        own_state[name].copy_(param.data)
+
 
 def main():
     args = parser.parse_args()
     print("=> creating pre-trained model '{}'".format(args.arch))
 
-    for dir in args.output_dir:
-        checkdir(dir)
+    # for dir in args.output_dir:
+    #     checkdir(dir)
 
-    pretrained_model = [models.__dict__[args.arch]() for path in args.pretrained]
+    save_img_dir = os.path.join("/home/comp/20481896/", "datasets/cifar10")
+    save_model_dir= os.path.join("/home/comp/20481896/", "DecoupleGradVariance/experiments/standalone/DecoupleGradVariance/checkpoints")
+    target_path = os.path.join(save_model_dir, "partition_alpha=0.1checkpoint-server-standalone-FedAvg-resnet18_v2-cifar10-128-0.1-no-hetero-0.1-False-epoch-999-default.pth")
+    args.output_dir = save_model_dir
+    checkdir(args.output_dir)
+
+    # pretrained_model = [models.__dict__[args.arch]() for path in args.pretrained]
+    pretrained_model = ResNet18(None, 10)
+    ckp_T = torch.load(target_path)
+    load_my_state_dict(pretrained_model, ckp_T)
 
     # load from pre-trained, before DistributedDataParallel constructor
-    for pi,path in enumerate(args.pretrained):
-        if os.path.isfile(path):
-            print("=> loading checkpoint '{}'".format(path))
-            checkpoint = torch.load(path, map_location="cpu")
+    # for pi,path in enumerate(args.pretrained):
+    #     if os.path.isfile(path):
+    #         print("=> loading checkpoint '{}'".format(path))
+    #         checkpoint = torch.load(path, map_location="cpu")
 
-            try:
-                state_dict = checkpoint['model']
-            except:
-                try:
-                    state_dict = checkpoint['state_dict']
-                except:
-                    state_dict = checkpoint
-            for k in list(state_dict.keys()):
-                if k.startswith('fc.'):
-                    del state_dict[k]
-                elif k.startswith('module.fc.'):
-                    del state_dict[k]
-                elif k.startswith('module'):
-                    state_dict[k.replace('module.','')]=state_dict[k]
-                    del state_dict[k]
+    #         try:
+    #             state_dict = checkpoint['model']
+    #         except:
+    #             try:
+    #                 state_dict = checkpoint['state_dict']
+    #             except:
+    #                 state_dict = checkpoint
+    #         for k in list(state_dict.keys()):
+    #             if k.startswith('fc.'):
+    #                 del state_dict[k]
+    #             elif k.startswith('module.fc.'):
+    #                 del state_dict[k]
+    #             elif k.startswith('module'):
+    #                 state_dict[k.replace('module.','')]=state_dict[k]
+    #                 del state_dict[k]
 
-            args.start_epoch = 0
-            msg = pretrained_model[pi].load_state_dict(state_dict, strict=False)
-            print(msg.missing_keys)
-            if len(msg.missing_keys):
-                assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}
+    #         args.start_epoch = 0
+    #         msg = pretrained_model[pi].load_state_dict(state_dict, strict=False)
+    #         print(msg.missing_keys)
+    #         if len(msg.missing_keys):
+    #             assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}
 
-            print("=> loaded pre-trained model '{}'".format(path))
-        else:
-            print("=> no checkpoint found at '{}'".format(path))
-        pretrained_model[pi].cuda().eval()
+    #         print("=> loaded pre-trained model '{}'".format(path))
+    #     else:
+    #         print("=> no checkpoint found at '{}'".format(path))
+    #     pretrained_model[pi].cuda().eval()
 
-    for pi in range(len(args.pretrained)):
-        for name, param in pretrained_model[pi].named_parameters():
-            param.requires_grad = False
+    # for pi in range(len(args.pretrained)):
+    #     for name, param in pretrained_model[pi].named_parameters():
+    #         param.requires_grad = False
 
-    dir = os.path.join(args.data, 'val')
-    dataset = myDataLoader(dir)
+    pretrained_model.cuda().eval()
+    for name, param in pretrained_model.named_parameters():
+        param.requires_grad = False
 
-    dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers)
+    # dir = os.path.join(args.data, 'val')
+    # dataset = myDataLoader(dir)
+    # dataloader = torch.utils.data.DataLoader(
+    #     dataset, batch_size=args.batch_size, shuffle=False,
+    #     num_workers=args.workers)
+
+    (
+        train_data_num,
+        test_data_num,
+        train_data_global,
+        test_data_global,
+        train_data_local_num_dict,
+        train_data_local_dict,
+        test_data_local_dict,
+        class_num,
+    ) = efficient_load_partition_data_cifar10(
+        "cifar10",
+        save_img_dir,
+        "hetero",
+        0.1,
+        10,
+        128,
+        0,
+    )
+    mal_client_data = train_data_local_dict[0]
 
     criterion = nn.MSELoss().cuda()
     criterion2 = TVLoss().cuda()
     input_depth = 32
     imsize_net = 256
-    imsize = 224
+    # imsize = 224
+    imsize = 32
 
-    for i, (img, target, filename) in enumerate(dataloader):
-        if 1:
+    # # for i, (img, target, filename) in enumerate(mal_client_data):
+    # for i, (img, target) in enumerate(mal_client_data):
+    #     # if 1:
+    #     if i > 0:
+    #         # measure data loading time
+    #         img = img.cuda()
+            
+    #         # for pi in range(len(args.pretrained)):
+    #         targets = pretrained_model(img, name=args.which_layer).detach()
+    #         for img_i in range(img.size()[0]):
+    #             # if 1:
+    #             if i > 10:
+    #                 # out_path = os.path.join(args.output_dir, args.which_layer, filename[img_i])
+    #                 real_path = os.path.join(args.output_dir, args.which_layer, f"RealIMG{[img_i]}.png")
+    #                 out_path = os.path.join(args.output_dir, args.which_layer, f"reconIMG{[img_i]}.png")
+
+    #                 real_img = postp(img[img_i].data.cpu().squeeze())
+    #                 real_img.save(real_path)
+    #                 if not os.path.exists(out_path):
+    #                     print('%d-%d'%(i,img_i))
+    #                     start=time.time()
+    #                     pad = 'zero'  # 'refection'
+    #                     net = skip(input_depth, 3, num_channels_down=[16, 32, 64, 128, 128, 128],
+    #                                 num_channels_up=[16, 32, 64, 128, 128, 128],
+    #                                 num_channels_skip=[4, 4, 4, 4, 4, 4],
+    #                                 filter_size_down=[7, 7, 5, 5, 3, 3], filter_size_up=[7, 7, 5, 5, 3, 3],
+    #                                 upsample_mode='nearest', downsample_mode='avg',
+    #                                 need_sigmoid=False, pad=pad, act_fun='LeakyReLU').type(img.type())
+
+    #                     net_input = get_noise(input_depth, imsize_net).type(img.type()).detach()
+    #                     out = net(net_input)[:, :, :imsize, :imsize]
+    #                     print(out.size())
+
+    #                     # Compute number of parameters
+    #                     s = sum(np.prod(list(p.size())) for p in net.parameters())
+    #                     print('Number of params: %d' % s)
+
+    #                     target = targets[[img_i,],...]
+
+    #                     # run style transfer
+    #                     max_iter = args.max_iter
+    #                     show_iter = 50
+    #                     optimizer = optim.Adam(get_params('net', net, net_input), lr=args.lr)
+    #                     n_iter = [0]
+
+    #                     while n_iter[0] <= max_iter:
+
+    #                         def closure():
+    #                             optimizer.zero_grad()
+    #                             out = pretrained_model(net(net_input)[:, :, :imsize, :imsize], name=args.which_layer)
+    #                             loss = criterion(out, target) #+ criterion2(net_input)*1e-2
+    #                             loss.backward()
+    #                             n_iter[0] += 1
+    #                             # print loss
+    #                             if n_iter[0] % show_iter == (show_iter - 1):
+    #                                 print('Iteration: %d, loss: %f' % (n_iter[0] + 1, loss.item()))
+    #                             return loss
+
+    #                         optimizer.step(closure)
+    #                     out_img = postp(net(net_input)[:, :, :imsize, :imsize].data[0].cpu().squeeze())
+    #                     # plt.imshow(out_img)
+    #                     # plt.show()
+    #                     end = time.time()
+    #                     print('Time:'+str(end-start))
+
+    #                     checkdir(os.path.dirname(out_path))
+    #                     out_img.save(out_path)
+
+
+    # for batch_i, (img, target, filename) in enumerate(mal_client_data):
+    for batch_i, (img, target) in enumerate(mal_client_data):
+        # if 1:
+        if batch_i < 3:
             # measure data loading time
             img = img.cuda()
             
-            for pi in range(len(args.pretrained)):
-                targets = pretrained_model[pi](img, name=args.which_layer).detach()
-                for img_i in range(img.size()[0]):
-                    if 1:
-                        out_path = os.path.join(args.output_dir[pi], args.which_layer, filename[img_i])
-                        if not os.path.exists(out_path):
-                            print('%d-%d'%(i,img_i))
-                            start=time.time()
-                            
-                            pad = 'zero'  # 'refection'
-                            net = skip(input_depth, 3, num_channels_down=[16, 32, 64, 128, 128, 128],
-                                       num_channels_up=[16, 32, 64, 128, 128, 128],
-                                       num_channels_skip=[4, 4, 4, 4, 4, 4],
-                                       filter_size_down=[7, 7, 5, 5, 3, 3], filter_size_up=[7, 7, 5, 5, 3, 3],
-                                       upsample_mode='nearest', downsample_mode='avg',
-                                       need_sigmoid=False, pad=pad, act_fun='LeakyReLU').type(img.type())
+            # for pi in range(len(args.pretrained)):
+            targets = pretrained_model(img, name=args.which_layer).detach()
+            # out_path = os.path.join(args.output_dir, args.which_layer, filename[batch_i])
+            out_path = os.path.join(args.output_dir, args.which_layer, f"reconIMG{[batch_i]}-mean.png")
 
-                            net_input = get_noise(input_depth, imsize_net).type(img.type()).detach()
-                            out = net(net_input)[:, :, :224, :224]
-                            print(out.size())
+            for img_i in range(5):
+                real_path = os.path.join(args.output_dir, args.which_layer, f"RealIMG{[batch_i]}-{img_i}.png")
+                real_img = postp(img[img_i].data.cpu().squeeze())
+                real_img.save(real_path)
 
-                            # Compute number of parameters
-                            s = sum(np.prod(list(p.size())) for p in net.parameters())
-                            print('Number of params: %d' % s)
+            real_path = os.path.join(args.output_dir, args.which_layer, f"RealIMG{[batch_i]}-mean.png")
+            real_img = postp(img.mean(dim=0).data.cpu().squeeze())
+            real_img.save(real_path)
 
-                            target = targets[[img_i,],...]
+            target = targets.mean(dim=0)
+            # noised_targets = [target]
+            DP_degree = [0, 0.001, 0.01, 0.1, 0.5]
+            for i, std in enumerate(DP_degree):
+                # noised_targets.append[target + torch.normal(mean=torch.ones(target.shape)*0.0, std=std)]
+                if std > 0.0:
+                    noised_target = target + torch.normal(mean=torch.ones(target.shape)*0.0, std=std).cuda()
+                else:
+                    noised_target = target + 0.0
+                out_path = os.path.join(args.output_dir, args.which_layer, f"reconIMG{[batch_i]}-mean-DP{std}.png")
+                if not os.path.exists(out_path):
+                    print('%d-%d'%(batch_i, batch_i))
+                    start=time.time()
+                    pad = 'zero'  # 'refection'
+                    net = skip(input_depth, 3, num_channels_down=[16, 32, 64, 128, 128, 128],
+                                num_channels_up=[16, 32, 64, 128, 128, 128],
+                                num_channels_skip=[4, 4, 4, 4, 4, 4],
+                                filter_size_down=[7, 7, 5, 5, 3, 3], filter_size_up=[7, 7, 5, 5, 3, 3],
+                                upsample_mode='nearest', downsample_mode='avg',
+                                need_sigmoid=False, pad=pad, act_fun='LeakyReLU').type(img.type())
 
-                            # run style transfer
-                            max_iter = args.max_iter
-                            show_iter = 50
-                            optimizer = optim.Adam(get_params('net', net, net_input), lr=args.lr)
-                            n_iter = [0]
+                    net_input = get_noise(input_depth, imsize_net).type(img.type()).detach()
+                    out = net(net_input)[:, :, :imsize, :imsize]
+                    print(out.size())
 
-                            while n_iter[0] <= max_iter:
+                    # Compute number of parameters
+                    s = sum(np.prod(list(p.size())) for p in net.parameters())
+                    print('Number of params: %d' % s)
 
-                                def closure():
-                                    optimizer.zero_grad()
-                                    out = pretrained_model[pi](net(net_input)[:, :, :imsize, :imsize], name=args.which_layer)
-                                    loss = criterion(out, target) #+ criterion2(net_input)*1e-2
-                                    loss.backward()
-                                    n_iter[0] += 1
-                                    # print loss
-                                    if n_iter[0] % show_iter == (show_iter - 1):
-                                        print('Iteration: %d, loss: %f' % (n_iter[0] + 1, loss.item()))
-                                    return loss
 
-                                optimizer.step(closure)
-                            out_img = postp(net(net_input)[:, :, :imsize, :imsize].data[0].cpu().squeeze())
-                            # plt.imshow(out_img)
-                            # plt.show()
-                            end = time.time()
-                            print('Time:'+str(end-start))
+                    # run style transfer
+                    max_iter = args.max_iter
+                    show_iter = 50
+                    optimizer = optim.Adam(get_params('net', net, net_input), lr=args.lr)
+                    n_iter = [0]
 
-                            checkdir(os.path.dirname(out_path))
-                            out_img.save(out_path)
+                    while n_iter[0] <= max_iter:
+
+                        def closure():
+                            optimizer.zero_grad()
+                            out = pretrained_model(net(net_input)[:, :, :imsize, :imsize], name=args.which_layer)
+                            loss = criterion(out, noised_target) #+ criterion2(net_input)*1e-2
+                            loss.backward()
+                            n_iter[0] += 1
+                            # print loss
+                            if n_iter[0] % show_iter == (show_iter - 1):
+                                print('Iteration: %d, loss: %f' % (n_iter[0] + 1, loss.item()))
+                            return loss
+
+                        optimizer.step(closure)
+                    out_img = postp(net(net_input)[:, :, :imsize, :imsize].data[0].cpu().squeeze())
+                    # plt.imshow(out_img)
+                    # plt.show()
+                    end = time.time()
+                    print('Time:'+str(end-start))
+
+                    checkdir(os.path.dirname(out_path))
+                    out_img.save(out_path)
 
 
 if __name__ == '__main__':
